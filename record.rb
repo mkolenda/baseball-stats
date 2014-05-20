007aa0784787464318aefde5dfcda986cb649ee7
Matt Kolenda <mkolenda@gmail.com>
2014-05-20 19:19:39 -0400

    moved the ::most_improved method to the parent class.  Others may want to use it in the future

--- record.rb
+++ record.rb
@@ -187,5 +187,32 @@ class Record < Hash
         end
       end
     end
+
+    def most_improved(year, attribute)
+      diff = prev_val = cur_val = 0
+      mi = []
+      found = find(year: year)
+      if found
+        found.each do |record|
+          p = find(player_id: record.player_id, year: year - 1)
+          if p
+            cur_val = record.send(attribute)
+            prev_val = p[0].send(attribute)
+
+            next if prev_val == 0
+            # calculate the percentage change from the previous year
+            d = (cur_val - prev_val) / prev_val
+
+            if d > diff
+              mi = [record]
+              diff = d
+            elsif d == diff
+              mi << record
+            end
+          end
+        end
+      end
+      [mi, diff]
+    end
   end
 end
--- stat_player_year.rb
+++ stat_player_year.rb
@@ -1,6 +1,6 @@
 #
 # Stores data summarized by player and year
-#
+# Will be used to compute most improved batting average 
 
 require_relative 'record'
 require_relative 'baseball_stats'
@@ -9,31 +9,4 @@ class StatPlayerYear < Record
   include BaseballStats
   @records = {}
   @keys = [:player_id, :year]
-
-  def self.most_improved(year, attribute)
-    diff = prev_val = cur_val = 0
-    mi = []
-    found = find(year: year)
-    if found
-      found.each do |record|
-        p = find(player_id: record.player_id, year: year - 1)
-        if p
-          cur_val = record.send(attribute)
-          prev_val = p[0].send(attribute)
-
-          next if prev_val == 0
-          # calculate the percentage change from the previous year
-          d = (cur_val - prev_val) / prev_val
-
-          if d > diff
-            mi = [record]
-            diff = d
-          elsif d == diff
-            mi << record
-          end
-        end
-      end
-    end
-    [mi, diff]
-  end
 end
